3da7da7a2894508607534e58568edaa8fbf816db
Andryellysson <143898683+Andryellysson@users.noreply.github.com>
2024-11-07 21:19:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class aula06
+{
+    static void Main()
+    {
+
+        double ValorCompra;
+        double ValorVenda;
+        double Lucro = 0.2;
+        string Produto = "Nutella";
+        double qtdProduto = 15;
+        double ValorPacotao = 30;
+
+        ValorCompra = ValorPacotao / qtdProduto;
+        ValorVenda = ValorCompra + (ValorCompra * Lucro);
+
+        Console.WriteLine("Valor do Pacotão: {0:c}", ValorPacotao);
+        Console.WriteLine("Quantidade de Pacotinhos: {0}", qtdProduto);
+        Console.WriteLine("Produto: {0}", Produto);
+        Console.WriteLine("Valor de cada produto: {0:c}", ValorCompra);
+        Console.WriteLine("Lucro: {0:p}", Lucro);
+        Console.WriteLine("Valor da Venda: {0:c}", ValorVenda);
+
+    }
+}
